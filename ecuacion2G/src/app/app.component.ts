f324ffa8e204d12e57e4ea5d4430dcf03b523404
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-15 12:19:20 +0100

    primera version

---
+++ ecuacion2G/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ecuacion2G';
+
+  coeficientes = {
+    grado2: 0,
+    grado1: 0,
+    independiente: 0,
+  };
+
+  discriminante: number = 0;
+  raizdiscriminante: number = 0;
+  advertencia: string = '¿Que ecuación deseas resolver?'
+
+  solucion = {
+    solucion1: 0,
+    solucion2: 0,
+  };
+
+
+
+  calcular() {
+
+    this.discriminante = this.coeficientes.grado1 ** 2 - 4 * this.coeficientes.grado2 * this.coeficientes.independiente;
+    this.raizdiscriminante = Math.sqrt(this.discriminante);
+
+    if (this.discriminante < 0) {
+      this.advertencia = 'no existe solución real';
+    } else {
+      this.solucion.solucion1 = (- this.coeficientes.grado1 + this.raizdiscriminante) / 2 * this.coeficientes.grado2;
+
+      this.solucion.solucion2 = (- this.coeficientes.grado1 - this.raizdiscriminante) / 2 * this.coeficientes.grado2;
+
+    };
+
+  }
+
+
+}
